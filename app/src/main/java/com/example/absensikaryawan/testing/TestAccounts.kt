21faedec1540cd1d5f5c94e0df62f55899cadca8
uddind <qomarudinmohammad9@gmail.com>
2026-08-19 16:12:59 +0700

    Update test accounts

--- app/src/main/java/com/example/absensikaryawan/testing/TestAccounts.kt
+++ app/src/main/java/com/example/absensikaryawan/testing/TestAccounts.kt
@@ -1,4 +1,4 @@
-package com.example.absensikaryawan.testing
+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package com.example.absensikaryawan.testing
 
 object TestAccounts {
 
